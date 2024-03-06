ee96e2bea0cd3df367f8d9d8b2f117b81d25ee15
Lucas <lucasdslvra@gmail.com>
2024-03-06 16:34:36 +0100

    start

---
+++ Drunkard/Models/ab.swift
@@ -0,0 +1,20 @@
+//
+//  ab.swift
+//  Drunkard
+//
+//  Created by da silveira lucas on 06/03/2024.
+//
+
+import SwiftUI
+
+struct ab: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ab_Previews: PreviewProvider {
+    static var previews: some View {
+        ab()
+    }
+}
---
+++ Drunkard/Views/a.swift
@@ -0,0 +1,20 @@
+//
+//  a.swift
+//  Drunkard
+//
+//  Created by da silveira lucas on 06/03/2024.
+//
+
+import SwiftUI
+
+struct a: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct a_Previews: PreviewProvider {
+    static var previews: some View {
+        a()
+    }
+}
