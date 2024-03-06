087da2fca565fb3f3bd64fc3c490844526162d6c
Nathan BOSCHI <nathanboschi25@gmail.com>
2024-03-06 16:16:58 +0100

    Initial Commit

---
+++ Drunkard/DrunkardApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrunkardApp.swift
+//  Drunkard
+//
+//  Created by boschi nathan on 06/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct DrunkardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
