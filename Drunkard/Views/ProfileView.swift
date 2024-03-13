88774a7af6ba9ea79ce312fac0618b768791dc32
Lucas D <lucasdslvra@gmail.com>
2024-03-13 15:50:56 +0100

    Update profile

--- Drunkard/Views/ProfileView.swift
+++ Drunkard/Views/ProfileView.swift
@@ -12,19 +12,48 @@ struct ProfileView: View {
     @State var FieldText: String = ""
     @State private var selection = ""
     @State private var age: Int = 1
+    @State private var taille: Int = 1
+    @State private var poids: Int = 1
     
     let genders = ["Male", "Female", "Autre"]
     
     var body: some View {
         
         VStack{
+            Spacer()
+            
             Image(systemName: "person.crop.circle")
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 100, height: 100)
             
+            Spacer()
+            
             HStack{
                 Text("Age : ")
+                Picker("Age ", selection: $age){
+                    ForEach(16...100, id: \.self){ age in
+                        Text("\(age) ans")
+                    }
+                }
+            }
+            
+            HStack{
+                Text("Taille : ")
+                Picker("Taille ", selection: $taille){
+                    ForEach(140...220, id: \.self){ taille in
+                        Text("\(taille) cm")
+                    }
+                }
+            }
+            
+            HStack{
+                Text("Poids : ")
+                Picker("Poids ", selection: $poids){
+                    ForEach(40...250, id: \.self){ poids in
+                        Text("\(poids) kg")
+                    }
+                }
             }
             
             HStack{
@@ -34,8 +63,17 @@ struct ProfileView: View {
                                     Text($0)
                                 }
                             }
-                            .pickerStyle(.menu)
             }
+            
+            Spacer()
+            
+            Button {
+                
+            } label: {
+                Text("Save")
+            }.buttonStyle(.borderedProminent)
+
+            Spacer()
         }
     }
 }
