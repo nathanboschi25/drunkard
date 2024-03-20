a734627855f40ce0ea2012de9d5ccfc361af3e9c
Nathan BOSCHI <nathanboschi25@gmail.com>
2024-03-20 15:21:42 +0100

    on fait du ménage

--- Drunkard/Models/Drink.swift
+++ Drunkard/Models/Drink.swift
@@ -9,7 +9,12 @@ import Foundation
 
 enum DrinkVolume: Double, CaseIterable {
     case shot = 3
-    case wineglass = 10
+    case wine = 10
+    case beer = 25
+    case winebottle = 75
+    case liter = 100
+    case cubitainer = 500
+    case barrel = 1600
 }
 
 
