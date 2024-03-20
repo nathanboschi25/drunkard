4ed0169a2d428e23edb9035a7f4195ceea6aa185
Lucas <lucasdslvra@gmail.com>
2024-03-20 15:28:25 +0100

    My goal fini

--- Drunkard/Views/GoalView.swift
+++ Drunkard/Views/GoalView.swift
@@ -14,7 +14,7 @@ func date(year: Int, month: Int, day: Int = 1) -> Date {
 
 struct GoalView: View {
     
-    private var linePoints: [VerreMax] = [
+    private var linePointsVerre: [VerreMax] = [
         VerreMax(x: date(year: 2023, month: 10, day: 2), y: 4),
         VerreMax(x: date(year: 2023, month: 10, day: 3), y: 5),
         VerreMax(x: date(year: 2023, month: 10, day: 4), y: 2),
@@ -22,6 +22,16 @@ struct GoalView: View {
         VerreMax(x: date(year: 2023, month: 10, day: 6), y: 4),
         VerreMax(x: date(year: 2023, month: 10, day: 7), y: 8),
         VerreMax(x: date(year: 2023, month: 10, day: 8), y: 2),
+    ]
+    
+    private var linePointsTaux: [TauxMax] = [
+        TauxMax(x: date(year: 2023, month: 10, day: 2), y: 1.25),
+        TauxMax(x: date(year: 2023, month: 10, day: 3), y: 1.0),
+        TauxMax(x: date(year: 2023, month: 10, day: 4), y: 2.5),
+        TauxMax(x: date(year: 2023, month: 10, day: 5), y: 3),
+        TauxMax(x: date(year: 2023, month: 10, day: 6), y: 1.75),
+        TauxMax(x: date(year: 2023, month: 10, day: 7), y: 2.0),
+        TauxMax(x: date(year: 2023, month: 10, day: 8), y: 1.5),
     ]
     
     var body: some View {
@@ -43,11 +53,15 @@ struct GoalView: View {
                     .foregroundColor(.white)
                     .cornerRadius(10)
 
-                }
+                }.frame(maxWidth: 120, maxHeight: 150)
+                .foregroundColor(.white)
+                    .background(Color.red)
+                    .cornerRadius(10)
+                    
                 
                 VStack(spacing:20){
                     Text("Taux Max")
-                    Text("4.2 g/L").font(.title)
+                    Text("1.5 g/L").font(.title)
                     
                     Button {
                         print("Edit taux goal")
@@ -59,13 +73,17 @@ struct GoalView: View {
                     .background(.red)
                     .foregroundColor(.white)
                     .cornerRadius(10)
-                }
+                    
+                }.frame(maxWidth: 120, maxHeight: 150)
+                    .foregroundColor(.white)
+                        .background(Color.red)
+                        .cornerRadius(10)
                 
             }
                         
             TabView{
                 Chart {
-                    ForEach(linePoints) { point in
+                    ForEach(linePointsVerre) { point in
                         Plot {
                             LineMark(
                                 x: .value("Date", point.x),
@@ -76,8 +94,20 @@ struct GoalView: View {
                     }
                     RuleMark(y: .value("Verre max", 6)).foregroundStyle(.red).blur(radius: 2)
                 }.frame(maxHeight: 160).padding(.horizontal, 30)
-                Text("AAAAAAA")
-                Text("AAAA")
+                
+                Chart {
+                    ForEach(linePointsTaux) { point in
+                        Plot {
+                            LineMark(
+                                x: .value("Date", point.x),
+                                y: .value("Level", point.y)
+                            )
+                            PointMark(x: .value("Date", point.x), y: .value("Level", point.y))
+                        }
+                    }
+                    RuleMark(y: .value("Taux max", 1.5)).foregroundStyle(.red).blur(radius: 2)
+                }.frame(maxHeight: 160).padding(.horizontal, 30)
+                
             }.tabViewStyle(.page)
 
             
@@ -89,6 +119,12 @@ struct GoalView: View {
 }
 
 struct VerreMax: Identifiable {
+    var id = UUID()
+    var x: Date
+    var y: Double
+}
+
+struct TauxMax: Identifiable {
     var id = UUID()
     var x: Date
     var y: Double
